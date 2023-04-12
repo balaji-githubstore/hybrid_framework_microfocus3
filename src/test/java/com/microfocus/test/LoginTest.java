f7ec2d7c12bc8a966511d66310bb24b9e4031bce
balaji-githubstore <balaji0017@gmail.com>
2023-04-12 15:06:17 +0530

    Added DataProvider for invalid login test

--- src/test/java/com/microfocus/test/LoginTest.java
+++ src/test/java/com/microfocus/test/LoginTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -15,14 +16,34 @@ import java.time.Duration;
  * Test methods related to login scenario
  */
 public class LoginTest extends WebDriverWrapper {
-    @Test
-    public void invalidLoginTest() {
-        driver.findElement(By.name("username")).sendKeys("john");
-        driver.findElement(By.name("password")).sendKeys("john");
+
+    //Create a dataprovider and connect to @Test invalidLoginTest
+    //peter,admin123,Invalid credentials
+    //john,admin123,Invalid credentials
+
+    @DataProvider
+    public Object[][] invalidLoginData()
+    {
+        Object[][] arr=new Object[2][3];
+
+        arr[0][0]="peter";
+        arr[0][1]="admin123";
+        arr[0][2]="Invalid credentials";
+
+        arr[1][0]="john";
+        arr[1][1]="john123";
+        arr[1][2]="Invalid credentials";
+
+        return arr;
+    }
+    @Test(dataProvider = "invalidLoginData")
+    public void invalidLoginTest(String username,String password,String expectedError) {
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 
         String actualError = driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
-        Assert.assertEquals(actualError, "Invalid credentials");
+        Assert.assertEquals(actualError, expectedError);
     }
 
     @Test
