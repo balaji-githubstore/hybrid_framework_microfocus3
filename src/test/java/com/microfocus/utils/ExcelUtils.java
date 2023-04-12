369ca66ed4442b7aadc30e93073728d1cf31d5dc
balaji-githubstore <balaji0017@gmail.com>
2023-04-12 16:28:09 +0530

    Added ExcelUtils.java - getSheetIntoTwoDimArray

--- src/test/java/com/microfocus/utils/ExcelUtils.java
+++ src/test/java/com/microfocus/utils/ExcelUtils.java
@@ -1,20 +1,18 @@
-package com.microfocus.test;
+package com.microfocus.utils;
 
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class DemoExcelTest {
+public class ExcelUtils {
 
-    public static void main(String[] args) throws IOException {
-
-        FileInputStream file = new FileInputStream("test_data/orange_data.xlsx");
+    public static Object[][] getSheetIntoTwoDimArray(String fileDetail, String sheetname) throws IOException {
+        FileInputStream file = new FileInputStream(fileDetail);
         XSSFWorkbook book = new XSSFWorkbook(file);
-        XSSFSheet sheet = book.getSheet("invalidLoginTest");
+        XSSFSheet sheet = book.getSheet(sheetname);
 
         int rowCount = sheet.getPhysicalNumberOfRows();
         int cellCount = sheet.getRow(0).getPhysicalNumberOfCells();
@@ -30,5 +28,7 @@ public class DemoExcelTest {
         }
         book.close();
         file.close();
+
+        return arr;
     }
 }
